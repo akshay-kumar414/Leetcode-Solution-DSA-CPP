e3f99488ec88e36ed12081bf98b954291bb7221d
akshay-kumar414 <akshayjann02@gmail.com>
2026-09-20 12:18:54 +0530

    Time: 0 ms (100%), Space: 15.3 MB (37.57%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,14 +1,68 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+
         int n = nums.size();
-        for(int i =0;i<n;i++){
-            if(nums[i] == target){
-                return i;
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
             }
 
+            // Check mid Part 1 mein hai ya Part 2 mein
+
+            if (nums[mid] > nums[n - 1]) {
+
+                // Part 1
+
+                if (target > nums[n - 1]) {
+
+                    // target bhi Part 1 mein hai
+
+                    if (nums[mid] < target) {
+                        low = mid + 1;
+                    }
+                    else {
+                        high = mid - 1;
+                    }
+
+                }
+                else {
+
+                    // target Part 2 mein hai
+                    low = mid + 1;
+                }
+            }
+
+            else {
+
+                // Part 2
+
+                if (target <= nums[n - 1]) {
+
+                    // target bhi Part 2 mein hai
+
+                    if (nums[mid] < target) {
+                        low = mid + 1;
+                    }
+                    else {
+                        high = mid - 1;
+                    }
+
+                }
+                else {
+
+                    // target Part 1 mein hai
+                    high = mid - 1;
+                }
+            }
         }
+
         return -1;
-        
     }
 };
